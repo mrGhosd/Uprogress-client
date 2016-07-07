5887c50d33376393bd5bbc91ba89a466315f91e4
Vadim Sokoltsov <vforvad@gmail.com>
2016-07-07 23:57:20 +0300

    add header component

--- app/components/root/app/RootApp.jsx
+++ app/components/root/app/RootApp.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import RootHeader from 'root/header/RootHeader';
 
 export default class RootApp extends Component {
   render() {
     return (
       <div>
+        <RootHeader />
         Root component
         {React.cloneElement(this.props.children, this.props)}
       </div>
---
+++ app/components/root/header/RootHeader.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class RootHeader extends Component {
+  render() {
+    return(
+      <div>Header</div>
+    );
+  }
+}
