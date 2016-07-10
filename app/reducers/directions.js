e1a9d8d4ee9fedfa1a6e4796e2043a105fd63ea8
Vadim Sokoltsov <vforvad@gmail.com>
2016-07-10 18:26:09 +0300

    add some style fixes

--- app/reducers/directions.js
+++ app/reducers/directions.js
@@ -1,12 +1,10 @@
 const initialState = {
-  directions: []
+  list: []
 }
 
 export default function directions(state = initialState, action) {
   switch(action.type) {
     case 'GET_DIRECTIONS_LIST':
-    console.log(action.directions);
-      // state.directions = action.directions;
       return { ...state, list: action.directions }
     default:
       return state;
