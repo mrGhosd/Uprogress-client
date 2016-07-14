41f2ade70c38666b06bb395e50d81b61089a9bc4
Vadim Sokoltsov <vforvad@gmail.com>
2016-07-15 02:32:48 +0300

    remove console.log

--- app/components/navigation/Navigation.jsx
+++ app/components/navigation/Navigation.jsx
@@ -42,7 +42,6 @@ class Navigation extends Component {
 
   render() {
     const { directions } = this.props;
-    console.log(this.props);
     return (
       <div className={CN(css.navigation, 'Card', 'divine')}>
         <Link to='/directions/new' className="create-button">Add</Link>
