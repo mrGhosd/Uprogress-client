0af34995be2d0b42b7b78a74bcad12531ba0c619
Vadim Sokoltsov <vforvad@gmail.com>
2016-07-15 20:29:27 +0300

    add directions detail view

--- app/components/directions/detail/DirectionsDetail.jsx
+++ app/components/directions/detail/DirectionsDetail.jsx
@@ -1,24 +1,43 @@
-import React, { Component, PropTypes } from 'react';
+import css from './directionsDetail.styl';
 
-export default class DirectionsDetail extends Component {
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import CN from 'classnames';
+
+import  { getDirection } from 'actions/directions';
+
+class DirectionsDetail extends Component {
 
   static propTypes = {
-    params: PropTypes.object
+    params: PropTypes.object,
+    dispatch: PropTypes.func,
+    directions: PropTypes.object
   };
 
   static defaultProps = {
     params: {
       course_id: ''
+    },
+    dispatch: () => {},
+    directions: {
+      detail: {}
     }
   };
 
+  componentWillMount() {
+    this.props.dispatch(getDirection(this.props.params.course_id));
+  }
+
   render() {
-    const id = this.props.params.course_id;
+    const direction = this.props.directions.detail;
 
     return (
-      <div className="directions-detail">
-        Detail {id}
+      <div className={CN(css.directionsDetail)}>
+        <h1>{direction.title}</h1>
+        <p>{direction.description}</p>
       </div>
     );
   }
 }
+
+export default connect()(DirectionsDetail);
