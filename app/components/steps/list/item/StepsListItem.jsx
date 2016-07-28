ae85036ad30a1f3c1ccc5e7b8ee63185dcbadf19
Vadim Sokoltsov <vforvad@gmail.com>
2016-07-28 22:56:08 +0300

    add checkbox element and implement it on steps list

--- app/components/steps/list/item/StepsListItem.jsx
+++ app/components/steps/list/item/StepsListItem.jsx
@@ -1,4 +1,9 @@
+import css from './StepsListItem.styl';
+import CN from 'classnames';
+
 import React, { Component, PropTypes } from 'react';
+
+import CheckBox from 'CheckBox/ElementCheckBox';
 
 export default class StepsListItem extends Component {
 
@@ -10,12 +15,21 @@ export default class StepsListItem extends Component {
     step: {}
   };
 
+  displayCheckbox() {
+
+  }
+
+  displayTitle() {
+
+  }
+
   render() {
     const { step } = this.props;
-    
+
     return (
-      <div>
-        <p>{step.title}</p>
+      <div className={CN(css.stepsListItem)}>
+        <CheckBox checked={step.is_done} />
+        <div>{step.title}</div>
       </div>
     );
   }
---
+++ app/elements/CheckBox/ElementCheckBox.jsx
@@ -0,0 +1,60 @@
+import css from './ElementCheckBox.styl';
+import React, { Component } from 'react';
+
+export default class ElementCheckBox extends Component {
+
+  static defaultProps = {
+    name: '',
+    onChange: (() => {})
+  }
+
+  static propTypes = {
+    name: React.PropTypes.string,
+    id: React.PropTypes.string,
+    value: React.PropTypes.string,
+    disabled: React.PropTypes.bool,
+    checked: React.PropTypes.bool,
+    onChange: React.PropTypes.func,
+    label: React.PropTypes.string
+  }
+
+  /**
+   * Check if checkbox is checked
+   * @return {Boolean} Is checkbox cheched?
+   */
+  value(newcheck) {
+    if (typeof newcheck === 'boolean') {
+      this.refs.checkbox.checked = newcheck;
+    }
+    return this.refs.checkbox.checked;
+  }
+
+  setOption(optionName, options) {
+    if (this.props[optionName]) {
+      options[optionName] = optionName;
+    }
+    return options;
+  }
+
+
+  /**
+   * Render base element
+   */
+  render() {
+    const name = this.props.name;
+    const value = this.props.value;
+    const id = this.props.id;
+    const onChange = this.props.onChange;
+
+    let inputOptions = { type: 'checkbox', ref: 'checkbox', id, value, name, onChange };
+
+    inputOptions = this.setOption('disabled', inputOptions);
+    inputOptions = this.setOption('checked', inputOptions);
+
+    return (
+      <div className={css.CheckBox}>
+        <input {...inputOptions} />
+      </div>
+    );
+  }
+}
