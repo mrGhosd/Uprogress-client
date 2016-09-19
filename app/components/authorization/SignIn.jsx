0a8dd345819893a1ff155428386208186a77048a
vsokoltsov <vforvad@gmail.com>
2016-09-19 12:07:52 +0300

    add imposition for sign-in page

--- app/components/authorization/SignIn.jsx
+++ app/components/authorization/SignIn.jsx
@@ -1,11 +1,58 @@
+import css from './SignIn.styl';
+
 import React, { Component } from 'react';
+import CN from 'classnames';
 
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
+import TextField from 'TextField/ElementTextField';
+import Button from 'Button/ElementButton';
 
 class SignIn extends Component {
+
+  state = {
+    user: {
+      email: '',
+      password: ''
+    },
+    errors: {
+      email: [],
+      password: []
+    }
+  };
+
+  handleChange(event) {
+    let lastState = this.state.user;
+
+    lastState[event.target.name] = event.target.value;
+    this.setState({ user: lastState });
+  }
+
+  submitForm(event) {
+    console.log(event);
+  }
+
   render() {
+    const user = this.state.user;
+    const errors = this.state.errors;
+
     return (
-      <div>SignIn component</div>
+      <div className={CN(css.signIn, 'Card')}>
+        <form>
+          <TextField ref="email"
+            name="email"
+            value={user.email}
+            onChange={this::this.handleChange} error={errors.email} />
+          <TextField ref="password"
+            name="password"
+            type="password"
+            value={user.password}
+            onChange={this::this.handleChange} error={errors.password} />
+          <Button onClick={this::this.submitForm} color="blue">Sign in</Button>
+          <p>Don't have an account? <Link to="/sign_up">Sign up</Link></p>
+        </form>
+      </div>
     );
   }
 }
