4ea2a70caa1d78bc13cc21d3b08222732a70df83
vsokoltsov <vforvad@gmail.com>
2016-09-19 12:19:59 +0300

    implement sign up page

--- app/components/authorization/SignIn.jsx
+++ app/components/authorization/SignIn.jsx
@@ -1,4 +1,4 @@
-import css from './SignIn.styl';
+import css from './SignPage.styl';
 
 import React, { Component } from 'react';
 import CN from 'classnames';
@@ -38,15 +38,17 @@ class SignIn extends Component {
     const errors = this.state.errors;
 
     return (
-      <div className={CN(css.signIn, 'Card')}>
+      <div className={CN(css.signPage, 'Card')}>
         <form>
           <TextField ref="email"
             name="email"
+            placeholder="Email"
             value={user.email}
             onChange={this::this.handleChange} error={errors.email} />
           <TextField ref="password"
             name="password"
             type="password"
+            placeholder="Password"
             value={user.password}
             onChange={this::this.handleChange} error={errors.password} />
           <Button onClick={this::this.submitForm} color="blue">Sign in</Button>
--- app/components/authorization/SignUp.jsx
+++ app/components/authorization/SignUp.jsx
@@ -1,11 +1,74 @@
+import css from './SignPage.styl';
+
 import React, { Component } from 'react';
+import CN from 'classnames';
 
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
+import TextField from 'TextField/ElementTextField';
+import Button from 'Button/ElementButton';
 
 class SignUp extends Component {
+  state = {
+    user: {
+      email: '',
+      password: '',
+      passwordConfirmation: '',
+      nick: ''
+    },
+    errors: {
+      email: [],
+      password: [],
+      passwordConfirmation: [],
+      nick: []
+    }
+  };
+
+  handleChange(event) {
+    let lastState = this.state.user;
+
+    lastState[event.target.name] = event.target.value;
+    this.setState({ user: lastState });
+  }
+
+  submitForm(event) {
+    console.log(event);
+  }
+
   render() {
+    const user = this.state.user;
+    const errors = this.state.errors;
+
     return (
-      <div>SignUp component</div>
+      <div className={CN(css.signPage, 'Card')}>
+        <form>
+          <TextField ref="email"
+            name="email"
+            value={user.email}
+            placeholder="Email"
+            onChange={this::this.handleChange} error={errors.email} />
+          <TextField ref="password"
+            name="password"
+            type="password"
+            placeholder="Password"
+            value={user.password}
+            onChange={this::this.handleChange} error={errors.passwordConfirmation} />
+          <TextField ref="passwordConfirmation"
+            name="passwordConfirmation"
+            type="password"
+            placeholder="Password Confirmation"
+            value={user.passwordConfirmation}
+            onChange={this::this.handleChange} error={errors.passwordConfirmation} />
+          <TextField ref="nick"
+            name="nick"
+            placeholder="Nick"
+            value={user.nick}
+            onChange={this::this.handleChange} error={errors.nick} />
+          <Button onClick={this::this.submitForm} color="blue">Sign up</Button>
+          <p>Already have an account? <Link to="/sign_in">Sign in</Link></p>
+        </form>
+      </div>
     );
   }
 }
