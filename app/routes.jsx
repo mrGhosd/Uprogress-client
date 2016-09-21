ad26a2c6eee33db3f18a49c4a8ccf9f39870f51b
Vadim Sokoltsov <vforvad@gmail.com>
2016-09-21 19:23:23 +0300

    move redirection function into separated module

--- app/routes.jsx
+++ app/routes.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
+import { redirectFromRoot } from 'routes/redirects';
+
 import App from 'App.js';
 import RootIndex from 'root/index/RootIndex';
 import Dashboard from 'dashboard/Dashboard';
@@ -10,23 +12,8 @@ import SignIn from 'authorization/SignIn';
 import SignUp from 'authorization/SignUp';
 
 import { Provider } from 'react-redux';
+
 import store from './store';
-
-/**
- * Redirect from root path if user not signed in
- * @param  {Object} nextState state of the router
- * @param  {Function} replace helps to replace the route path
- * @param  {Function} callback make a callback after onEnter hook
- * @return {Dispatch} Dispatch function
- */
-function redirectFromRoot(nextState, replace, callback) {
-  const storeContent = store.getState();
-
-  if (storeContent.users.current.isEmpty) {
-    replace('/sign_in');
-  }
-  callback();
-}
 
 export default (
   <Provider store={store}>
@@ -39,7 +26,6 @@ export default (
             <IndexRoute component={Dashboard} />
             {Directions}
           </Route>
-
       </Route>
     </Router>
   </Provider>
--- app/routes/redirects.js
+++ app/routes/redirects.js
@@ -1,1 +1,17 @@
+import store from 'store';
 
+/**
+ * Redirect from root path if user not signed in
+ * @param  {Object} nextState state of the router
+ * @param  {Function} replace helps to replace the route path
+ * @param  {Function} callback make a callback after onEnter hook
+ * @return {Dispatch} Dispatch function
+ */
+export function redirectFromRoot(nextState, replace, callback) {
+  const storeContent = store.getState();
+
+  if (storeContent.users.current.isEmpty) {
+    replace('/sign_in');
+  }
+  callback();
+}
