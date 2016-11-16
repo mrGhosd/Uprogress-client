a32a86e4fdd6cdb5136268681caee7f1669fbf48
Vadim Sokoltsov <vforvad@gmail.com>
2016-11-17 01:40:55 +0300

    implement boilerplate for a correct dropdown

--- app/components/user/header/UserHeader.jsx
+++ app/components/user/header/UserHeader.jsx
@@ -3,7 +3,7 @@ import css from './UserHeader.styl';
 import CN from 'classnames';
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
-import Dropdown from 'Dropdown/ElementDropdown';
+import DropdownMenu from 'react-dd-menu';
 
 import { signOut } from 'actions/users';
 
@@ -12,6 +12,10 @@ import Image from 'Image/ElementImage';
 import SvgIcon from 'SVGIcon/SVGIcon';
 
 export default class UserHeader extends Component {
+
+  state = {
+    isOpen: false
+  };
 
   static propTypes = {
     user: PropTypes.object,
@@ -56,30 +60,57 @@ export default class UserHeader extends Component {
     );
   }
 
+  close() {
+    this.setState({ isMenuOpen: false });
+  }
+
+  toggle() {
+    let prevState = this.state;
+
+    prevState.isOpen = !prevState.isOpen
+    this.setState(prevState);
+  }
+
+  click() {
+    console.log('You clicked an item');
+  }
+
   render() {
     const { user } = this.props;
     const image = this.displayImage(user);
-    const dropDownItems = [
-      {
-        value: '1'
-      },
-      {
-        value: '2'
-      },
-      {
-        value: '3'
-      }
-    ];
+    // const dropDownItems = [
+    //   {
+    //     text: '111',
+    //     value: '1'
+    //   },
+    //   {
+    //     text: '222',
+    //     value: '2'
+    //   },
+    //   {
+    //     text: '333',
+    //     value: '3'
+    //   }
+    // ];
+    const menuOptions = {
+      isOpen: this.state.isOpen,
+      close: this.close.bind(this),
+      toggle: <button type="button" onClick={this.toggle.bind(this)}>Click me!</button>,
+      align: 'right',
+    };
+    console.log(menuOptions);
 
     return (
       <div className={CN(css.userHeader)}>
         <div className="user-nick">
-          <Dropdown items={dropDownItems}>
-            <span className="nick">
-              {image}
-              <span >{user.nick}</span>
-            </span>
-          </Dropdown>
+          <span className="nick">
+            {image}
+            <span >{user.nick}</span>
+          </span>
+          <DropdownMenu isOpen={this.state.isOpen} close={menuOptions.close} toggle={menuOptions.toggle}>
+            <li><a href="#">Example 1</a></li>
+            <li><button type="button" onClick={this.click.bind(this)}>Example 2</button></li>
+          </DropdownMenu>
         </div>
         <Button size="auto" color="red" onClick={this::this.signOut} className="sign-out-button">Sign out</Button>
       </div>
