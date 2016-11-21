a2ed976aed9608c2559a2c4db2b4353b4d1b7978
Vadim Sokoltsov <vforvad@gmail.com>
2016-11-21 18:49:01 +0300

    add some improvements

--- app/utils/iconsForAuthoirzations.js
+++ app/utils/iconsForAuthoirzations.js
@@ -1,11 +1,21 @@
 const clients = ['Chrome', 'Safari'];
 
+/**
+ * Return icon name
+ * @param  {Object} authorization authorization object
+ * @return {String}               icon name
+ */
 export function getIcon(authorization) {
   const client = matchedCliend(authorization);
 
   return `${client}_auth_icon`;
 }
 
+/**
+ * Return matched client name
+ * @param  {Object} authorization authorization object
+ * @return {String}               matched client name
+ */
 function matchedCliend(authorization) {
   const indexOfBrowser = clients.indexOf(authorization.appName);
 
