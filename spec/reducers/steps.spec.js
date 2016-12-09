ba984f906c9c7413c02596a1fa650492af6df2e2
Vadim Sokoltsov <vforvad@gmail.com>
2016-12-09 12:45:18 +0300

    add boilerplate for steps reducers specs

---
+++ spec/reducers/steps.spec.js
@@ -0,0 +1,34 @@
+import reducer from 'reducers/directions';
+import expect from 'expect';
+
+const initialState = {
+  list: [],
+  errors: {},
+  edit: {}
+};
+
+describe('Steps reducers', () => {
+  describe('DIRECTION', () => {
+
+  });
+
+  describe('EDIT_STEP', () => {
+
+  });
+
+  describe('CREATE_STEP', () => {
+
+  });
+
+  describe('UPDATE_STEP', () => {
+
+  });
+
+  describe('DELETE_STEP', () => {
+
+  });
+
+  describe('FAILED_OPERATION', () => {
+
+  });
+});
