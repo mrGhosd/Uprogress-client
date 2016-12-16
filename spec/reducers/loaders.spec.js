a1d647041dc9c0cdc661e58183fe674f51ce7c7f
Vadim Sokoltsov <vforvad@gmail.com>
2016-12-16 18:16:00 +0300

    add other specs for loader reducer actions

--- spec/reducers/loaders.spec.js
+++ spec/reducers/loaders.spec.js
@@ -3,22 +3,34 @@ import expect from 'expect';
 
 describe('Loaders reducers', () => {
   describe('START_MAIN_LOADER', () => {
-    it('returns updated list key', () => {
+    it('returns updated main key', () => {
       expect(
-        reducer( { main: true }, { type: 'START_MAIN_LOADER' })
+        reducer({ main: true }, { type: 'START_MAIN_LOADER' })
       ).toEqual({ main: false });
     });
   });
 
   describe('STOP_MAIN_LOADER', () => {
-
+    it('returns updated main key', () => {
+      expect(
+        reducer({ main: false }, { type: 'STOP_MAIN_LOADER' })
+      ).toEqual({ main: true });
+    });
   });
 
   describe('START_FILE_LOADER', () => {
-
+    it('returns updated file key', () => {
+      expect(
+        reducer({ file: true }, { type: 'START_FILE_LOADER' })
+      ).toEqual({ file: false });
+    });
   });
 
   describe('STOP_FILE_LOADER', () => {
-
+    it('returns updated file key', () => {
+      expect(
+        reducer({ file: false }, { type: 'STOP_FILE_LOADER' })
+      ).toEqual({ file: true });
+    });
   });
 });
