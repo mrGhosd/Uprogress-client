f64e0276ef33ce9a825cd2ad285b31f5cf984889
Vadim Sokoltsov <vforvad@gmail.com>
2016-12-17 01:44:07 +0300

    add specs for SIGN_UP_USER action in user's reducer

--- spec/reducers/users.spec.js
+++ spec/reducers/users.spec.js
@@ -12,7 +12,7 @@ describe('Users reducers', () => {
     });
 
     it('saves token in localStorage', () => {
-      reducer({ signInErrors: {} }, { type: 'SIGN_IN_USER', token })
+      reducer({ signInErrors: {} }, { type: 'SIGN_IN_USER', token });
       expect(localStorage.getItem('uprogresstoken')).toEqual(token);
     });
   });
@@ -22,7 +22,18 @@ describe('Users reducers', () => {
   });
 
   describe('SIGN_UP_USER', () => {
+    const token = '12345';
 
+    it('return updated list key', () => {
+      expect(
+        reducer({ signUpErrors: {} }, { type: 'SIGN_UP_USER', token })
+      ).toEqual({ signUpErrors: {} });
+    });
+
+    it('saves token in localStorage', () => {
+      reducer({ signUpErrors: {} }, { type: 'SIGN_UP_USER', token });
+      expect(localStorage.getItem('uprogresstoken')).toEqual(token);
+    });
   });
 
   describe('SIGN_UP_FAILED', () => {
