86b43b358f3fbc36bca22ef953ad5f88589e88bd
Vadim Sokoltsov <vforvad@gmail.com>
2016-12-16 18:13:47 +0300

    add spec for START_MAIN_LOADER reducer

--- spec/reducers/loaders.spec.js
+++ spec/reducers/loaders.spec.js
@@ -1,9 +1,13 @@
-import reducer from 'reducers/steps';
+import reducer from 'reducers/loaders';
 import expect from 'expect';
 
 describe('Loaders reducers', () => {
   describe('START_MAIN_LOADER', () => {
-
+    it('returns updated list key', () => {
+      expect(
+        reducer( { main: true }, { type: 'START_MAIN_LOADER' })
+      ).toEqual({ main: false });
+    });
   });
 
   describe('STOP_MAIN_LOADER', () => {
