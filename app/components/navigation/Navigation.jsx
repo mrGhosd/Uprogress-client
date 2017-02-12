f024aea044859f0c2d629486cabbcfd6ea132a8a
Vadim Sokoltsov <vforvad@gmail.com>
2017-02-12 07:28:18 +0300

    fix issue with selecting tabs from finished directions list

--- app/components/navigation/Navigation.jsx
+++ app/components/navigation/Navigation.jsx
@@ -42,14 +42,16 @@ export class Navigation extends Component {
     let result;
 
     if (this.props.params && this.props.params.id) {
-      result = this.props.params.id == item.id;
+      const directionId = this.props.params.id;
+
+      result = directionId == item.id || directionId == item.slug;
     }
     return result;
   }
 
   render() {
     const { directions, user, currentUser } = this.props;
-
+    
     return (
       <div className={CN(css.navigation, 'Card', 'divine')}>
         {currentUser.id === user.id && <Link to={`/${user.nick}/directions/new`} className="create-button">Add</Link>}
