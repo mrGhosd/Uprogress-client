73fbe29a9e932cfb2eed2f8f66449e7cf51e98e1
Vadim Sokoltsov <vforvad@gmail.com>
2017-04-10 13:22:37 +0300

    improve appointments list item imposition

--- app/components/appointments/list/item/AppointmentListItem.jsx
+++ app/components/appointments/list/item/AppointmentListItem.jsx
@@ -1,6 +1,10 @@
-import React, { Component, PropTypes } from 'react';
+import css from './AppointmentListItem.styl';
 
+import React, { Component, PropTypes } from 'react';
+import CN from 'classnames';
 import moment from 'moment';
+
+import SvgIcon from 'SVGIcon/SVGIcon';
 
 export default class AppointmentListItem extends Component {
   static propTypes = {
@@ -17,8 +21,13 @@ export default class AppointmentListItem extends Component {
     const { appointment } = this.props;
 
     return (
-      <div>
-        {moment(appointment.date).format('dddd, MMMM Do, YYYY h:mm')}
+      <div className={CN(css.appointmentListItem)}>
+        <div className="icon">
+          <SvgIcon icon="sand_clock_icon" />
+        </div>
+        <div className="info">
+          {moment(appointment.date).format('dddd, MMMM Do, YYYY h:mm')}
+        </div>
       </div>
     );
   }
