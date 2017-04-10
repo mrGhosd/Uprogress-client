d7e701633eeb7a567088b3c805d06624f5353876
Vadim Sokoltsov <vforvad@gmail.com>
2017-04-10 13:40:01 +0300

    add different views for appointment list item

--- app/components/appointments/list/item/AppointmentListItem.jsx
+++ app/components/appointments/list/item/AppointmentListItem.jsx
@@ -17,17 +17,46 @@ export default class AppointmentListItem extends Component {
     dispatch: () => {}
   };
 
+  getIcon(appointment) {
+    let icon;
+
+    if (appointment.available) {
+      icon = 'sand_clock_icon';
+    }
+    else {
+      icon = 'icon-success';
+    }
+
+    return <SvgIcon icon={icon} />;
+  }
+
+  formtDate(appointment) {
+    const validDate = moment(appointment.date).format('dddd, MMMM Do, YYYY h:mm');
+    let template;
+
+    if (appointment.available) {
+      template = validDate;
+    }
+    else {
+      template = (<strike>{validDate}</strike>);
+    }
+
+    return template;
+  }
+
   render() {
     const { appointment } = this.props;
+    const date = this.formtDate(appointment);
+    const icon = this.getIcon(appointment);
 
     return (
       <div className={CN(css.appointmentListItem)}>
         <div className="icon">
-          <SvgIcon icon="sand_clock_icon" />
+          {icon}
         </div>
         <div className="info">
           <div className="date">
-            {moment(appointment.date).format('dddd, MMMM Do, YYYY h:mm')}
+            {date}
           </div>
           <div className="message">
             {appointment.message && appointment.message}
